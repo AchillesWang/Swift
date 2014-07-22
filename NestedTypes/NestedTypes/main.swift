a1fa5475779b6f663a8c46dfd8d4183ec3b6d0ea
汪潇翔 <wang19900820@gmail.com>
2014-07-22 17:23:57 +0800

    NestedTypes(类型嵌套)
    类型嵌套

---
+++ NestedTypes/NestedTypes/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  NestedTypes
+//
+//  Created by WangXiaoXiang on 14-7-22.
+//  Copyright (c) 2014年 Next. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
