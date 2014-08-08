8198ef03603b93730b9b76467bd5232f7c220f1e
汪潇翔 <wang19900820@gmail.com>
2014-08-08 18:07:50 +0800

    泛型

--- Generics/Generics/main.swift
+++ Generics/Generics/main.swift
@@ -60,4 +60,102 @@ stackOfStrings.push("韩庆辉")
 println("items:\(stackOfStrings.items)")
 
 
+func findStringIndex(array:[String],valueToFind:String) -> Int?{
+    for (index,value) in enumerate(array){
+        if value == valueToFind {
+            return index
+        }
+    }
+    return nil
+}
+
+func findIndex<T:Equatable>(array:[T],valueToFind:T) -> Int? {
+    for (index,value) in enumerate(array){
+        if value == valueToFind {
+            return index
+        }
+    }
+    return nil
+}
+let strings = ["cat", "dog", "llama", "parakeet", "terrapin"];
+
+let index = findStringIndex(strings, "terrapin")
+
+
+let Nos = [1,2,3,4,5,6,7,8,9,0];
+
+let index2 = findIndex(Nos, 0)
+
+let index3 = findIndex([1,2,3], 3.1415926)
+
+
+protocol Container{
+    typealias ItemType
+    mutating func append(item:ItemType)
+    var count:Int{ get }
+    subscript(i:Int) -> ItemType{ get }
+}
+
+struct IntStact:Container{
+    var items = [Int]()
+    mutating func append(item: Int){
+        items.append(item)
+    }
+    var count:Int{
+    return items.count
+    }
+    subscript(i:Int) -> Int{
+        return items[i]
+    }
+}
+
+struct Stact<T>:Container{
+    var items = [T]()
+    mutating func append(item: T){
+        items.append(item)
+    }
+    var count:Int{
+    return items.count
+    }
+    subscript(i:Int) -> T{
+        return items[i]
+    }
+}
+
+func allItemsMatch<c1:Container,c2:Container where c1.ItemType == c2.ItemType,c1.ItemType:Equatable>(someContainer:c1,anotherContainer:c2) -> Bool{
+    // 检查两个Container的元素个数是否相同
+    if someContainer.count != anotherContainer.count{
+        return false
+    }
+    for i in 0...someContainer.count-1{
+        if someContainer[i] != anotherContainer[i]{
+            return false
+        }
+        
+    }
+    return true
+}
+
+
+var s1 = Stact<String>()
+s1.append("wxx")
+s1.append("wxk")
+s1.append("qy")
+
+var s2 = Stact(items: ["wxx","wxk","qy"])
+
+let array = ["wxx","dl","rfs"]
+
+println("s1:\(s1.items)")
+
+let m1 = allItemsMatch(s1, s2)
+
+
+if allItemsMatch(s1, s2){
+    println("一样")
+}else{
+    println("不同")
+}
+
+
 
